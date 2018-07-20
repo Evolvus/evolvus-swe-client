6c608d45304fa0a8930aaea65992c7fbc896ef33
pavithra30 <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-07-20 16:14:24 +0530

    remove .npmrc file

--- index.js
+++ index.js
@@ -1,66 +1,61 @@
 const debug = require("debug")("evolvus-swe-client:index");
 const axios = require("axios");
-// var querystring = require('querystring');
-
-
-
+var querystring = require('querystring');
+var SWE_URL = process.env.SWE_URL || "http://localhost:3000/api/swe";
 var TIME_OUT = process.env.TIME_OUT || 5000;
-
-// module.exports.postToSWE = (sweSetupObject) => {
-//   return new Promise((resolve, rejewfEntity: 'Role',
-//     try {
-//       var SWE_URL = process.env.SWE_URL || "http://localhost:3000/api/setup";
-//       debug("SWE_URL", SWE_URL);
-//       if (sweSetupObject == null || sweSetupObject == undefined) {
-//         debug(`IllegalArgument:Object is ${sweSetupObject}`);
-//         resolve(`IllegalArgument:Object is ${sweSetupObject}`);
-//       }
-//       var instance = axios.create({
-//         baseURL: SWE_URL,
-//         timeout: TIME_OUT
-//       });
-//       console.log("before post");
-//       instance.post(SWE_URL, querystring.stringify(sweSetupObject)).then((response) => {
-//         console.log("response", response);
-//         debug("RESPONSE", response.data);
-//         resolve(response.data);wfEntity: 'Role',
-//       }).catch((error) => {
-//         debug(`Error:${error} and failed to store is`, sweSetupObject);
-//         resolve(error);
-//       });
-//     } catch (error) {
-//       debug(`caught exception ${error} and failed to store is`, sweSetupObject);
-//       resolve(error);
-//     }
-//   });
-// };
 
 module.exports.initialize = (sweEventObject) => {
   return new Promise((resolve, reject) => {
     try {
-      var SWE_URL = process.env.SWE_URL || "http://localhost:3000/api/swe";
-      debug("SWE_URL", SWE_URL);
+      let SWE_INITIALIZE_URL = SWE_URL + "/initialize";
+      debug("SWE_INITIALIZE_URL", SWE_INITIALIZE_URL);
       if (sweEventObject == null || sweEventObject == undefined) {
         debug(`IllegalArgument:Object is ${sweEventObject}`);
         resolve(`IllegalArgument:Object is ${sweEventObject}`);
       }
       var instance = axios.create({
-        baseURL: SWE_URL,
+        baseURL: SWE_INITIALIZE_URL,
         timeout: TIME_OUT
       });
 
-      instance.post(SWE_URL, sweEventObject).then((response) => {
-        console.log("post method");
-        console.log("response", response.data);
+      instance.post(SWE_INITIALIZE_URL, sweEventObject).then((response) => {
         debug("RESPONSE", response.data);
         resolve(response.data);
       }).catch((error) => {
-        console.log("error", error);
         debug(`Error:${error} and failed to store is`, sweEventObject);
         resolve(error);
       });
     } catch (error) {
       debug(`caught exception ${error} and failed to store is`, sweEventObject);
+      resolve(error);
+    }
+  });
+};
+
+module.exports.Complete = (sweObject) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let SWE_COMPLETE_URL = SWE_URL + "/complete";
+      //resolve(true);
+      debug("SWE_COMPLETE_URL", SWE_COMPLETE_URL);
+      if (sweObject == null || sweObject == undefined) {
+        debug(`IllegalArgument:Object is ${sweObject}`);
+        resolve(`IllegalArgument:Object is ${sweObject}`);
+      }
+      var instance = axios.create({
+        baseURL: SWE_COMPLETE_URL,
+        timeout: TIME_OUT
+      });
+
+      instance.post(SWE_COMPLETE_URL, querystring.stringify(sweObject)).then((response) => {
+        debug("RESPONSE", response.data);
+        resolve(response.data);
+      }).catch((error) => {
+        debug(`Error:${error} and failed to store is`, sweObject);
+        resolve(error);
+      });
+    } catch (error) {
+      debug(`caught exception ${error} and failed to store is`, sweObject);
       resolve(error);
     }
   });
--- test/index.js
+++ test/index.js
@@ -4,25 +4,16 @@ const chaiAsPromised = require("chai-as-promised");
 const expect = chai.expect;
 const shortid = require("shortid");
 
+
 chai.use(chaiAsPromised);
 
 let index = require("../index");
 
 describe('testing index.js', () => {
-  let sweSetupObject = {
-    tenantId: "T001",
-    wfEntity: "Role",
-    wfEntityAction: "create",
-    flowCode: "flow",
-    createdBy: "KamalaraniP"
-  };
-  // var id;
-  // id = savedObj._id;
-
   let sweEventObject = {
-    "tenantId": "IVL",
-    "wfEntity": "Role",
-    "wfEntityAction": "create",
+    "tenantId": "T001",
+    "wfEntity": "ENTITY",
+    "wfEntityAction": "CREATE",
     "query": "5b4f2ab570b1da7bbf936b52",
     "createdBy": "KamalaraniP"
   };
@@ -32,7 +23,7 @@ describe('testing index.js', () => {
     createdBy: "KamalaraniP"
   };
 
-  describe("testing with sweEvent objects", () => {
+  describe("testing with sweEvent objects for initialize", () => {
     beforeEach((done) => {
       process.env.SWE_URL = "http://localhost:3000/api/swe/initialize";
       done();
@@ -40,10 +31,6 @@ describe('testing index.js', () => {
 
     it('should save valid object to database', (done) => {
       var res = index.initialize(sweEventObject);
-      // expect(res)
-      //   .to.be.eventually.have.property("wfEntity")
-      //   .to.include("Role")
-      //   .notify(done);
       expect(res)
         .to.be.eventually.have.property('data')
         .to.include(sweEventObject)
@@ -51,7 +38,7 @@ describe('testing index.js', () => {
     });
 
     it('should respond with status code 400 if sweObject is invalid', (done) => {
-      var res = index.postToSWE(invalidsweEvent);
+      var res = index.initialize(invalidsweEvent);
       expect(res)
         .to.be.fulfilled.then((resp) => {
           expect(resp.response.status)
@@ -61,7 +48,7 @@ describe('testing index.js', () => {
     });
 
     it('should be resolved with IllegalArgument when input object is null', (done) => {
-      var res = index.postToSWE(null);
+      var res = index.initialize(null);
       expect(res)
         .to.be.eventually.include("IllegalArgument")
         .notify(done);
@@ -70,7 +57,61 @@ describe('testing index.js', () => {
 
     it('should be resolved with IllegalArgument when input object is undefined', (done) => {
       let input;
-      var res = index.postToSWE(input);
+      var res = index.initialize(input);
+      expect(res)
+        .to.be.eventually.include("IllegalArgument")
+        .notify(done);
+    });
+  });
+
+  describe("testing with sweEvent objects for complete", () => {
+    beforeEach((done) => {
+      process.env.SWE_URL = "http://localhost:3000/api/swe/complete";
+      done();
+    });
+
+    let wfInstanceId = shortid.generate();
+    let sweObject = {
+      "tenantId": "T001",
+      "wfInstanceId": wfInstanceId,
+      "wfEntity": "ENTITY",
+      "wfEntityAction": "CREATE",
+      "query": wfInstanceId,
+      "createdBy": "KamalaraniP",
+      "wfEvent": "complete",
+      "comments": "complete"
+    };
+
+
+    it('should save valid object to database', (done) => {
+      var res = index.Complete(sweObject);
+      expect(res)
+        .to.be.eventually.have.property('data')
+        .to.include(sweObject)
+        .notify(done);
+    });
+
+    it('should respond with status code 400 if sweObject is invalid', (done) => {
+      var res = index.Complete(invalidsweEvent);
+      expect(res)
+        .to.be.fulfilled.then((resp) => {
+          expect(resp.response.status)
+            .to.be.eql(400);
+          done();
+        });
+    });
+
+    it('should be resolved with IllegalArgument when input object is null', (done) => {
+      var res = index.Complete(null);
+      expect(res)
+        .to.be.eventually.include("IllegalArgument")
+        .notify(done);
+    });
+
+
+    it('should be resolved with IllegalArgument when input object is undefined', (done) => {
+      let input;
+      var res = index.Complete(input);
       expect(res)
         .to.be.eventually.include("IllegalArgument")
         .notify(done);
@@ -79,12 +120,12 @@ describe('testing index.js', () => {
 
   describe("testing when the server is down", () => {
     beforeEach((done) => {
-      process.env.SWE_URL = "http://localhost:4000/api/setup";
+      process.env.SWE_URL = "http://localhost:4000/api/swe";
       done();
     });
 
     it('should respond with connection refused if workflow engine is down', (done) => {
-      var res = index.postToSWE(sweSetupObject);
+      var res = index.initialize(sweObject);
       expect(res)
         .to.be.fulfilled.then((resp) => {
           expect(resp.code)
